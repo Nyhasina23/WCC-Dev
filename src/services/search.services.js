7c1c3a92fcc6c5dfff5c2794882fbeec1f4bcaa4
NY-HASINA <nyhasina.finaritra@gmail.com>
2022-09-04 11:56:58 +0200

    wcc

---
+++ src/services/search.services.js
@@ -0,0 +1,63 @@
+const axios = require('axios')
+
+
+async function getUsers(createdObj ,username , location , page){
+    let created = '';
+    if(createdObj.year){
+        created = createdObj.year
+        if(createdObj.month){
+            created = created+'-'+createdObj.month
+            if(createdObj.day){
+                created = created+'-'+createdObj.day
+            }
+        }
+    }
+
+    if(!page) {
+        page = 1 ;
+    }
+    let response = {
+        status : 200 , 
+        data : undefined
+    };
+    if(createdObj.year){
+        await axios.get(`https://api.github.com/search/users?q=${username}+in:login+created:${created}+location:${location}&sort=joined&per_page=20&page=${page}` , {
+            headers : {
+                'Authorization' : 'Bearer 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'
+            }
+        }).then((res) => {
+    
+            response.status = res.status
+            response.data = res.data
+        }).catch(error => {
+            response.status = error.response.status
+        })
+    }else{
+        await axios.get(`https://api.github.com/search/users?q=${username}+in:login+location:${location}&sort=joined&per_page=20&page=${page}` , {
+        headers : {
+            'Authorization' : 'Bearer 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'
+        }
+    }).then((res) => {
+
+        response.status = res.status
+        response.data = res.data
+    }).catch(error => {
+        response.status = error.response.status
+    })
+    }
+    
+    return response;
+}
+
+export {getUsers}
+// console.log('running ')
+// async function test(){
+
+//     for(let i=0; i<31; i++){
+//         console.log(i);
+//         let res = await getUsers({year:'2022' , month : '05'} , 'ei' , 'france' , 1)
+//         console.log(res.status);
+//     }
+// } 
+
+// test()
